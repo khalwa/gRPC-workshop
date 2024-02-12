718cdab67028c15e055de354de32bfc41e649e59
Krzysztof Halwa <krzysztofhalwa@outlook.com>
2024-02-12 10:45:24 +0100

    Implementation for Task1.

---
+++ ManagementService/Services/HelloService.cs
@@ -0,0 +1,17 @@
+﻿using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using ManagementGrpc;
+
+namespace ManagementService.Services
+{
+    public class HelloService : Hello.HelloBase
+    {
+        public override Task<HelloResponse> SayHello(Empty request, ServerCallContext context)
+        {
+            return Task.FromResult(new HelloResponse
+            {
+                Response = "Hello World!"
+            });
+        }
+    }
+}

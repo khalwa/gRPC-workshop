74623f159abd89dadf709bfeb3edacbb7569939b
Krzysztof Halwa <krzysztofhalwa@outlook.com>
2024-02-28 10:07:36 +0100

    Implemented Task-2

---
+++ Client/Program.cs
@@ -0,0 +1,10 @@
+﻿using Grpc.Net.Client;
+using ManagementGrpc;
+using Google.Protobuf.WellKnownTypes;
+
+Console.WriteLine("Hello, World!");
+
+var channel = GrpcChannel.ForAddress("http://localhost:5000");
+var client = new Hello.HelloClient(channel);
+
+Console.WriteLine(client.SayHello(new Empty()));
--- ManagementService/Program.cs
+++ ManagementService/Program.cs
@@ -1,3 +1,5 @@
+using ManagementService.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Additional configuration is required to successfully run gRPC on macOS.
@@ -10,5 +12,7 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+
+app.MapGrpcService<HelloService>();
 
 app.Run();
